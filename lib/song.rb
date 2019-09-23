2495a0479596b9fe02cfb6f26b64e7305179a2aa
Dominique Skaf <doskaf@yahoo.com>
2019-09-23 17:51:45 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,10 +1,19 @@
 class Song
   
-  attr_accessor :name
+  attr_accessor :name, :artist
   
   def initialize(name)
     @name = name
+    @@all = []
+    @@all << self
   end
   
+  def all
+    @@all
+  end
+  
+  def artist
+    @artist
+  end
   
 end
